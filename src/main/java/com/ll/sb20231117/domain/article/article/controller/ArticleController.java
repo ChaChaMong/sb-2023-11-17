a0792413c1083620e0dc6a7e30a9075464f743b4
unknown <thflf93@gmail.com>
2023-11-18 23:55:18 +0900

    feat: 리스트,수정,삭제 기능 추가

--- src/main/java/com/ll/sb20231117/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231117/domain/article/article/controller/ArticleController.java
@@ -2,10 +2,16 @@ package com.ll.sb20231117.domain.article.article.controller;
 
 import com.ll.sb20231117.domain.article.article.entity.Article;
 import com.ll.sb20231117.domain.article.article.service.ArticleService;
-import com.ll.sb20231117.global.reData.RsData;
+import com.ll.sb20231117.global.rq.Rq;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,34 +19,85 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 public class ArticleController {
     private final ArticleService articleService;
-    private final List<Article> articles;
+    private final Rq rq;
+
+    @GetMapping("/article/list")
+    String showList(Model model) {
+        List<Article> articles = articleService.findAll();
+
+        model.addAttribute("articles", articles);
+
+
+        return "article/article/list";
+    }
+
+    @GetMapping("/article/detail/{id}")
+    String showDetail(Model model, @PathVariable long id) {
+        Article article = articleService.findById(id).get();
+
+        model.addAttribute("article", article);
+
+        return "article/article/detail";
+    }
 
     @GetMapping("/article/write")
     String showWrite() {
         return "article/article/write";
     }
 
+    @Data
+    public static class WriteForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+    }
+
     @PostMapping("/article/write")
-    @ResponseBody
-    RsData<Article> write(
-            String title,
-            String body
-    ){
-        Article article = articleService.write(title, body);
+    String write(@Valid WriteForm writeForm){
+        Article article = articleService.write(writeForm.title, writeForm.body);
 
-        RsData<Article> rs = new RsData<>(
-                "S-1",
-                "%d번 게시물이 작성되었습니다.".formatted(article.getId()),
-                article
-        );
+        String msg = "id %d, article created".formatted(article.getId());
 
-        String resultCode = rs.getResultCode();
-        String msg = rs.getMsg();
-        Article _article = rs.getData();
+        return "redirect:/article/list?msg=" + msg;
+    }
 
-        return rs;
+    @GetMapping("/article/modify/{id}")
+    String showModify(Model model, @PathVariable long id) {
+        Article article = articleService.findById(id).get();
+
+        model.addAttribute("article", article);
+
+        return "article/article/modify";
+    }
+
+    @Data
+    public static class ModifyForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+    }
+
+    @PostMapping("/article/modify/{id}")
+    String modify(@PathVariable long id, @Valid ModifyForm modifyForm){
+        articleService.modify(id, modifyForm.title, modifyForm.body);
+
+        String msg = "id %d, article modified".formatted(id);
+
+        return "redirect:/article/list?msg=" + msg;
+    }
+
+    @GetMapping("/article/delete/{id}")
+    String delete(@PathVariable long id) {
+        articleService.delete(id);
+
+        String msg = "id %d, article deleted".formatted(id);
+
+        return "redirect:/article/list?msg=" + msg;
     }
 
     @GetMapping("/article/getLastArticle")
--- src/main/java/com/ll/sb20231117/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb20231117/domain/article/article/repository/ArticleRepository.java
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ArticleRepository {
-    private final List<Article> articles = new ArrayList<>();
+    private final List<Article> articles = new ArrayList<>(){{
+        add(new Article(1L,"제목1", "내용1"));
+        add(new Article(2L,"제목2", "내용2"));
+        add(new Article(3L,"제목3", "내용3"));
+    }};
 
     public void save(Article article) {
         if (article.getId() == null) {
@@ -24,5 +29,15 @@ public class ArticleRepository {
 
     public List<Article> findAll() {
         return articles;
+    }
+
+    public Optional<Article> findById(long id) {
+        return articles.stream()
+                .filter(article -> article.getId() == id)
+                .findFirst();
+    }
+
+    public void delete(long id) {
+        articles.removeIf(article -> article.getId() == id);
     }
 }
