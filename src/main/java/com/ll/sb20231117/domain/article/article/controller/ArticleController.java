f60f2d9147b5f145906e4a94b350e1658cfab17c
unknown <thflf93@gmail.com>
2023-11-24 09:35:12 +0900

    refactor: DELETE, PUT 적용

--- src/main/java/com/ll/sb20231117/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/sb20231117/domain/article/article/controller/ArticleController.java
@@ -10,22 +10,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@Validated
+@RequestMapping("/article")
 public class ArticleController {
     private final ArticleService articleService;
     private final Rq rq;
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     String showList(Model model) {
         List<Article> articles = articleService.findAll();
         model.addAttribute("articles", articles);
@@ -33,7 +29,7 @@ public class ArticleController {
         return "article/article/list";
     }
 
-    @GetMapping("/article/detail/{id}")
+    @GetMapping("/detail/{id}")
     String showDetail(Model model, @PathVariable long id) {
         Article article = articleService.findById(id).get();
 
@@ -43,7 +39,7 @@ public class ArticleController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/article/write")
+    @GetMapping("/write")
     String showWrite() {
         return "article/article/write";
     }
@@ -57,7 +53,7 @@ public class ArticleController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @PostMapping("/article/write")
+    @PostMapping("/write")
     String write(@Valid WriteForm writeForm){
         Article article = articleService.write(rq.getMember(), writeForm.title, writeForm.body);
 
@@ -65,7 +61,7 @@ public class ArticleController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/article/modify/{id}")
+    @GetMapping("/modify/{id}")
     String showModify(Model model, @PathVariable long id) {
         Article article = articleService.findById(id).get();
 
@@ -85,7 +81,7 @@ public class ArticleController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @PostMapping("/article/modify/{id}")
+    @PutMapping("/modify/{id}")
     String modify(@PathVariable long id, @Valid ModifyForm modifyForm){
         Article article = articleService.findById(id).get();
 
@@ -97,7 +93,7 @@ public class ArticleController {
     }
 
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/article/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     String delete(@PathVariable long id) {
         Article article = articleService.findById(id).get();
 
@@ -106,17 +102,5 @@ public class ArticleController {
         articleService.delete(article);
 
         return rq.redirect("/article/list", "%d번 게시물 삭제되었습니다.".formatted(id));
-    }
-
-    @GetMapping("/article/getLastArticle")
-    @ResponseBody
-    Article getLastArticle() {
-        return articleService.findLastArticle();
-    }
-
-    @GetMapping("/article/getArticles")
-    @ResponseBody
-    List<Article> getArticles() {
-        return articleService.findAll();
     }
 }
