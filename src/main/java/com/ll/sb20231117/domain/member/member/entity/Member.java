54303927a8f79f8c7e7a2217112ff8a36239bc92
unknown <thflf93@gmail.com>
2023-11-28 14:38:53 +0900

    feat: jpa 기능 도입

--- src/main/java/com/ll/sb20231117/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/sb20231117/domain/member/member/entity/Member.java
@@ -1,26 +1,28 @@
 package com.ll.sb20231117.domain.member.member.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 
-@AllArgsConstructor
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
 @Getter
 @Setter
-@ToString
+@Builder
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String username;
     private String password;
-
-    public Member(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 
     public boolean isAdmin() {
         return username.equals("admin");
